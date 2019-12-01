5fd56b8a9c3c2ddc68c616fe692582e11537594f
BluebirdLyrase <38246181+BluebirdLyrase@users.noreply.github.com>
2019-12-01 08:35:02 +0700

    firebase login

--- app/src/main/java/com/example/newhelloandroid/MainActivity.java
+++ app/src/main/java/com/example/newhelloandroid/MainActivity.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.EditText;
 import android.util.Log;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -19,12 +23,13 @@ import com.google.firebase.firestore.QuerySnapshot;
 public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
-
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
         testFirestore();
     }
 
@@ -54,19 +59,41 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void Login(View view) {
+        final String TAG = "signInClick";
+        String email = ((TextView)findViewById(R.id.name)).getText().toString();
+        String password = ((TextView)findViewById(R.id.password)).getText().toString();
+        final Intent intent = new Intent(this, Restaurant.class);
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(getApplicationContext(),"Log in success",Toast.LENGTH_SHORT).show();
+                            startActivity(intent);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(getApplicationContext(),"Log in Fail",Toast.LENGTH_SHORT).show();
 
-        Intent intent = new Intent(this, Restaurant.class);
-        EditText editText2 = (EditText) findViewById(R.id.name);
-        String name = editText2.getText().toString();
-        EditText editText3 = (EditText) findViewById(R.id.password);
-        String password = editText3.getText().toString();
+                        }
+                    }
+                });
 
-        if(name.equals("user")&&password.equals("1234")){
-            startActivity(intent);
-            Toast.makeText(getApplicationContext(),"Loged in",Toast.LENGTH_SHORT).show();
-        }else{
-            Toast.makeText(getApplicationContext(),"Rock in Fail",Toast.LENGTH_SHORT).show();
-        }
+
+//        EditText editText2 = (EditText) findViewById(R.id.name);
+//        String name = editText2.getText().toString();
+//        EditText editText3 = (EditText) findViewById(R.id.password);
+//        String password = editText3.getText().toString();
+//
+//        if(name.equals("user")&&password.equals("1234")){
+//            startActivity(intent);
+//            Toast.makeText(getApplicationContext(),"Loged in",Toast.LENGTH_SHORT).show();
+//        }else{
+//            Toast.makeText(getApplicationContext(),"Rock in Fail",Toast.LENGTH_SHORT).show();
+//        }
     }
 
 
